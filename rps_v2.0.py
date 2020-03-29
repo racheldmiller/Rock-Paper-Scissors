5ec08748052368126b792cefd8ec95b5cf34348a
racheldmiller <rachelmiller415@gmail.com>
2020-03-29 16:50:42 -0700

    version 2

---
+++ rps_v2.0.py
@@ -0,0 +1,28 @@
+# ----------------------------- v2.0 code --------------------------------
+
+print("..........Rock..........")
+print("..........Paper..........")
+print("..........Scissors..........")
+
+player1 = input("Player 1, make your move: ")
+player2 = input("Player 2, make your move: ")
+
+if player1 == player2:
+    print("It's a tie!")
+elif player1 == "rock":
+    if player2 == "scissors":
+        print("Player 1 wins!")
+    elif player2 == "paper":
+        print("Player 2 wins!")
+elif player1 == "paper":
+    if player2 == "rock":
+        print("Player 1 wins!")
+    elif player2 == "scissors":
+        print("Player 2 wins!")
+elif player1 == "scissors":
+    if player2 == "paper":
+        print("Player 1 wins!")
+    elif player2 == "rock":
+        print("Player 2 wins!")
+else:
+    print("Uh-oh. Something's wrong.")
