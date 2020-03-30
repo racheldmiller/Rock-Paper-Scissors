105b691414ba189d7d119006f4130f5fb3c2a815
racheldmiller <rachelmiller415@gmail.com>
2020-03-29 17:02:44 -0700

    version 2

--- rps_v2.0.py
+++ rps_v2.0.py
@@ -4,10 +4,10 @@ print("..........Rock..........")
 print("..........Paper..........")
 print("..........Scissors..........")
 
-player1 = input("Player 1, make your move: ")
+player1 = input("Player 1, choose rock, paper, or scissors: ")
 # prevent Player 2 from seeing Player 1's response
 print("*** NO CHEATING!\n" * 20)
-player2 = input("Player 2, make your move: ")
+player2 = input("Player 2, choose rock, paper, or scissors: ")
 
 # Note: if it's a tie, then no need to check for all the other logic listed.
 if player1 == player2:
